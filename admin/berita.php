e4a09f12fb58f9c462d5da1e9022b7d94cde177d
romeiaAlexander <79191176+romeiaAlexander@users.noreply.github.com>
2023-06-18 01:21:19 +0800

    Sedikit Kejahilan (#15)
    Co-authored-by: albanijohanes <101566380+albanijohanes@users.noreply.github.com>

--- admin/berita.php
+++ admin/berita.php
@@ -5,8 +5,9 @@ if (isset($_POST['submit'])) {
     $judul_berita = $_POST['judul_berita'];
     $deskripsi_berita = $_POST['deskripsi_berita'];
 
-    $query = "INSERT INTO riversky (judul_berita, deskripsi_berita) VALUES ('$judul_berita', '$deskripsi_berita')";
-    if (mysqli_query($conn, $query)) {
+    $query = "INSERT INTO berita (judul_berita, deskripsi_berita) VALUES ('$judul_berita', '$deskripsi_berita')";
+    if (mysqli_query($conn, $query)) 
+    {
         header('Location: index.php?admin=berita');
         exit();
     } else {
